20d363ff27d19b3f2b73ac53472145084e5d70f9
Sam Kiptoo <samwelkanda@gmail.com>
2020-11-23 20:05:21 +0300

    Add views and templates for blog app

--- mysite/apps/blog/models.py
+++ mysite/apps/blog/models.py
@@ -1,10 +1,15 @@
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth import get_user_model
 
 # Create your models here.
 
 User = get_user_model()
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status='published')
 
 class Post(models.Model):
     STATUS_CHOICES = (
@@ -23,10 +28,22 @@ class Post(models.Model):
                             default='draft')
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    publish = models.DateTimeField(default=timezone.now) 
+    publish = models.DateTimeField(default=timezone.now)
+
+    objects = models.Manager() # The default manager
+    published = PublishedManager()
 
     class Meta:
         ordering = ('-publish',)
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                        args=[
+                            self.publish.year,
+                            self.publish.month,
+                            self.publish.day,
+                            self.slug,
+                        ])
