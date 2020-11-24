13af7e9dd248d4bd2c1dbbb0abd3df9a07d4e828
Sam Kiptoo <samwelkanda@gmail.com>
2020-11-24 14:03:13 +0300

    Addtags to blog posts

--- mysite/apps/blog/views.py
+++ mysite/apps/blog/views.py
@@ -1,4 +1,9 @@
+from taggit.models import Tag
+
 from django.core.mail import send_mail
+from django.core.paginator import Paginator, EmptyPage,\
+                                  PageNotAnInteger
+from django.db.models import Count
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 
@@ -6,6 +11,32 @@ from.forms import CommentForm, EmailPostForm
 from .models import Comment, Post
 
 # Create your views here.
+
+def post_list(request, tag_slug=None):
+    object_list = Post.published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
+    paginator = Paginator(object_list, 3) # 3 posts in each page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+    return render(request,
+                 'blog/post/list.html',
+                 {'page': page,
+                  'posts': posts,
+                  'tag': tag})
+
+
 class PostListView(ListView):
     queryset = Post.published.all()
     context_object_name = 'posts'
