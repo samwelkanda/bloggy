91598cc49645eab9bb65ba40c7700a8839698430
Sam Kiptoo <samwelkanda@gmail.com>
2020-11-25 20:46:06 +0300

    Add selenium and functional tests

---
+++ functional_tests.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+
+browser = webdriver.Firefox()
+
+# Samwel has heard about a cool new blog. He goes to
+# check out its homepage
+
+browser.get('http://0.0.0:8000')
+
+# He notices the page header and title mention blog application.
+
+assert 'My Blog' in browser.title
+
+# He finds an interesting post on python
+# He click it right away and starts to read it
+# He finds it interesting and decides to comment on how great
+# the post is
+# After liking the post so much he decides to share it with his
+# friend Silas
+# He checks out a recommended post on Django
+
+browser.quit()
