bc23d1fb5e35ed98ceadd2544307247f469e6681
Sam Kiptoo <samwelkanda@gmail.com>
2020-11-28 21:46:05 +0300

    Add test for homepage

--- mysite/apps/blog/tests.py
+++ mysite/apps/blog/tests.py
@@ -1,7 +1,16 @@
 from django.test import TestCase
+from django.urls import resolve
+
+from .views import post_list
 
 # Create your tests here.
 class SmokeTest(TestCase):
 
     def test_monkey_math(self):
-        self.assertEqual(1 + 2, 4)
+        self.assertEqual(1 + 2, 3)
+
+class BlogHomePageTest(TestCase):
+
+    def test_blog_homepage_resolves(self):
+        found = resolve('/blog/')
+        self.assertEqual(found.func, post_list)
